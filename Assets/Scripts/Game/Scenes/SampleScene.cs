062812366176fd645ffacec6623ac8811dc721b0
ipwoo <wesley_woo@yeah.net>
2021-01-20 22:12:23 +0800

    init folder struct

---
+++ Assets/Scripts/Game/Scenes/SampleScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		var myLoadedAssetBundle =
+			AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "test_bundle"));
+		if (myLoadedAssetBundle == null)
+		{
+			Debug.Log("Failed to load AssetBundle!");
+			return;
+		}
+
+		var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("MyObject");
+		Instantiate(prefab);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
