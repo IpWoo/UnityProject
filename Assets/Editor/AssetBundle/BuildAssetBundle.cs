1dafd1799084837807ad0fe7bf6123ec6e7b9186
wuye <wuye@hetao101.com>
2021-01-21 11:56:39 +0800

    update gltf support

--- Assets/Editor/AssetBundle/BuildAssetBundle.cs
+++ Assets/Editor/AssetBundle/BuildAssetBundle.cs
@@ -14,6 +14,6 @@ public class CreateAssetBundles
 		}
 		BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, 
 			BuildAssetBundleOptions.UncompressedAssetBundle, 
-			BuildTarget.StandaloneWindows);
+			BuildTarget.StandaloneOSX);
 	}
 }
